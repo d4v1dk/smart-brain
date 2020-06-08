d33538c2b4debc83f69a9e8b05800edf97ca43dc
David Kovacs <davidkovacsmail@gmail.com>
2020-06-08 12:16:47 +0200

    components built [252]

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import './App.css';
+import Particles from 'react-particles-js';
+
+import Navigation from './components/navigation/Navigation';
+import Logo from './components/logo/Logo';
+import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
+import Rank from './components/rank/Rank';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 250
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      
+      <Particles 
+        params={particlesOptions}
+        className='particles'
+      />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/*<FaceRecognition />*/}
     </div>
   );
 }
